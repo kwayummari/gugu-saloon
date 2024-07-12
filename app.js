4f144fa6dfd9b9e53ff530fdc9b8cf68ef279be0
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-12 03:48:25 +0300

    fixed

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const morgan = require('morgan');
 const routes = require('./routes/routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const dotenv = require('dotenv');
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use helmet middleware for setting HTTP headers
 app.use(
   helmet({
     contentSecurityPolicy: {
@@ -22,34 +22,42 @@ app.use(
         scriptSrc: ["'self'"],
         styleSrc: ["'self'"],
         imgSrc: ["'self'"],
-        // Add more directives as needed
       },
     },
+    referrerPolicy: { policy: 'no-referrer' },
+    xssFilter: true,
+    hidePoweredBy: true,
+    frameguard: { action: 'deny' },
+    hsts: {
+      maxAge: 63072000,
+      includeSubDomains: true,
+      preload: true,
+    },
+    noSniff: true,
+    ieNoOpen: true,
   })
 );
 
-// Use CORS middleware with specific options
 app.use(cors({
-  // origin: 'http://trusted-origin.com',
   origin: '*',
-  methods: ['GET', 'POST'],
+  // origin: ['*', 'https://senjaro.app', 'http://localhost:3001'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 3000,
-  max: 1000,
+  windowMs: 15 * 60 * 1000,
+  max: 10000,
 });
 app.use(limiter);
 
 app.use(morgan('combined'));
 
-// Swagger Configuration
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: 'ERP',
-      description: 'This API provides endpoints to manage activities on an ERP platform. ' +
+      title: 'GUGU API',
+      description: 'This API provides endpoints to manage activities on an GUGU platform. ' +
       'It allows users to create, retrieve, update, and delete various learning activities such as ' +
       'courses, quizzes, assignments, discussions, and more. Authentication and user-related functionalities ' +
       'are also included to manage users enrolled in activities and their progress.',
@@ -61,18 +69,19 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api-senjaro', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Use routes defined in routes.js with base URL '/api'
 app.use('/', routes);
 
-// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Welcome to my Node.js API!');
+  res.send('Welcome To GUGU API!');
 });
 
-// Start the server
-const PORT = process.env.API_PORT || 8080;
+// const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.API_PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+PORT.keepAliveTimeout = process.env.API_KEEPALIVETIMEOUT; 
+PORT.headersTimeout = process.env.API_HEADERSTIMEOUT; 
