197704e6dbc35bc6288e72beca97c546d461550b
Kwayu Mmari <mmaridenis@gmail.com>
2024-09-05 16:15:29 +0300

    fixing galleru part

--- models/hairdresser/get_hairdressing2.js
+++ models/hairdresser/get_hairdressing2.js
@@ -5,17 +5,15 @@ const getHairDressing2 = async (req, res) => {
   try {
     const { editHairDresserId } = req.body;
     const connectionPool = await connectionPoolWithRetry();
-    connectionPool.query(queries.get_hairdressing2, ['38'], (error, results) => {
+    connectionPool.query(queries.get_hairdressing2, [editHairDresserId], (error, results) => {
       if (error) {
         console.error('Error fetching branch:', error);
         return res.status(500).json({ message: 'Internal Server Error' });
       }
 
       if (results.length === 0) {
-        console.log('Andrew ni boya sana')
         return res.status(404).json({ message: 'No hairdressing found' });
       }
-      console.log(results)
       res.status(200).json({ message: 'Hair dressing fetched successfully', hairDressing: results });
     });
   } catch (err) {
